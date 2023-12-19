302ef91f0cfcb7a7f308ea5c994bcc6e4491c108
leofer99 <44415118+leofer99@users.noreply.github.com>
2023-12-19 11:29:18 +0000

    Add files via upload

---
+++ templates/about_us_tpl.php
@@ -0,0 +1,9 @@
+<section>
+        <h2>Welcome to Paws & People!</h2>
+       
+    </section>
+    
+    <section>
+        <h2>About Us</h2>
+       
+    </section>
---
+++ templates/header_tpl.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/project_style.css"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paws & People Association</title>
+   
+</head>
+<body>
+    <header>
+        <h1>Paws & People Association</h1>
+
+        <?php if (isset($_SESSION['login_id'])) { ?>
+            <form id="logout" action="action_logout.php">
+            <span><?php echo $_SESSION['username'] ?></span>
+            <button>Logout</button>
+            </form>   
+        <?php } ?>
+
+
+        <nav>
+            <!-- Display membership status here -->
+            <ul>
+                <li><a href="project_homepage.php">Home</a></li>
+                <li><a href="project_about_us.php">About Us</a></li>
+                <li><a href="project_login.php">Login</a></li>
+                <li><a href="project_quotas.php"> Quotas Information </a></li>
+              </ul>
+    
+              <style> 
+              </style>
+        </nav>
+    </header>
