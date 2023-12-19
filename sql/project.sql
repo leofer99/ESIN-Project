aae5de6992980f43bee1381e1c29861f72853db3
leofer99 <44415118+leofer99@users.noreply.github.com>
2023-12-19 17:38:10 +0000

    Add files via upload

--- sql/project.sql
+++ sql/project.sql
@@ -7,7 +7,6 @@ PRAGMA foreign_keys= ON;
 DROP TABLE IF EXISTS MemberStorage;
 DROP TABLE IF EXISTS Storage;
 
-DROP TABLE IF EXISTS MemberFees;
 DROP TABLE IF EXISTS MemberPayment;
 
 DROP TABLE IF EXISTS Fees;
@@ -20,7 +19,6 @@ DROP TABLE IF EXISTS MemberHistory;
 DROP TABLE IF EXISTS AssociationHistory;
 
 DROP TABLE IF EXISTS Member;
-DROP TABLE IF EXISTS FeesInfo;
 DROP TABLE IF EXISTS Admin;
 DROP TABLE IF EXISTS Person;
 
@@ -32,22 +30,13 @@ CREATE TABLE Person(
    
 );
 
-CREATE TABLE FeesInfo(
-    id_feeinfo INTEGER PRIMARY KEY AUTOINCREMENT,
-    fee_type TEXT NOT NULL CHECK (fee_type='regular' OR fee_type='social bodies'),
-    --fee_status TEXT NOT NULL CHECK (fee_status='paid' OR fee_status='behind' OR fee_status='danger of expulsion'),
-    --fee_months_ahead INTEGER
-);
-
 CREATE TABLE Member (
     id_ INTEGER PRIMARY KEY REFERENCES Person,
     gender TEXT NOT NULL CHECK (gender='female' OR gender='male' OR gender='other'),
     city TEXT NOT NULL,
     joined_date TEXT NOT NULL,
     login_id INTEGER NOT NULL UNIQUE,
-    login_password TEXT NOT NULL,
-    id_feeinfo INTEGER REFERENCES FeesInfo --ON DELETE SET NULL ON UPDATE CASCADE
-
+    login_password TEXT NOT NULL
 );
 
 
@@ -73,7 +62,7 @@ CREATE TABLE MemberEvent(
 );
 
 CREATE TABLE Payment(
-   id_pay INTEGER PRIMARY KEY, 
+   id_pay INTEGER PRIMARY KEY AUTOINCREMENT, 
    type_payment TEXT NOT NULL 
    CHECK(type_payment='mbway' OR type_payment='bank transfer'), 
    date_payment TEXT NOT NULL, 
@@ -82,29 +71,23 @@ CREATE TABLE Payment(
 );
 
 CREATE TABLE Fees(
-    id_fee INTEGER PRIMARY KEY,
+    id_fee INTEGER PRIMARY KEY AUTOINCREMENT,
     fee_amount INTEGER NOT NULL DEFAULT 2.5,
-    fee_year INTEGER NOT NULL CHECK (fee_year>0),
+    fee_year INTEGER NOT NULL CHECK (fee_year>0)
     --fee_month INTEGER,  --months ahead they have paid for
-    id_pay INTEGER REFERENCES Payment
-);
-
-CREATE TABLE MemberFees(
-    id_fee INTEGER REFERENCES Fees,
-    login_id INTEGER REFERENCES Member
-   
+    --id_pay INTEGER REFERENCES Payment
 );
 
 CREATE TABLE MemberPayment(
-    id_pay INTEGER REFERENCES Payment,
-    login_id INTEGER REFERENCES Member
-   
+    login_id INTEGER REFERENCES Member,
+    id_pay INTEGER REFERENCES Payment
 );
 
 CREATE TABLE AssociationHistory(
-    id_asso INTEGER PRIMARY KEY,
+    id_asso INTEGER PRIMARY KEY AUTOINCREMENT,
     role_asso TEXT NOT NULL CHECK ( role_asso>0),
-    year_asso TEXT NOT NULL --ou text?
+    role_date_begin TEXT NOT NULL, 
+    role_date_end TEXT NOT NULL
    
 );
 
@@ -115,7 +98,7 @@ CREATE TABLE MemberHistory(
 );
 
 CREATE TABLE Storage(
-    sid INTEGER PRIMARY KEY,
+    sid INTEGER PRIMARY KEY AUTOINCREMENT,
     product_type TEXT NOT NULL
 
 );
