476c0ad44490af4160d62ab52970196ec1a604ef
leofer99 <44415118+leofer99@users.noreply.github.com>
2023-12-20 23:34:57 +0000

    Update about_us_tpl.php

--- Project/templates/about_us_tpl.php
+++ Project/templates/about_us_tpl.php
@@ -1,6 +1,9 @@
  <!-- About Us Section -->
- <section class="about-us-section">
+ <section class="hero-section">
         <div class="container">
+         <h2>Being Paws & People</h2>
+            <div>
+              <p>We are experts in emotional support, emphatic and highly trained therapists, focused on rehabilitation and emotional support to patients with chronic and terminal conditions and their families. Our mission is promote physical and emotional well-being through the human-dog bond.</p>
             <h2>We are Paws & People!</h2>
             <div class="pet-card">
                 <img src="images/chav1.jpg" alt="Description of Photo 1" style="width: 350px; height: 400px; object-fit: cover;">
@@ -9,10 +12,24 @@
             </div>
         </div>
     </section>
+ <section class="hero-section">
+    </section>
+    
 
     <!-- Therapy Pets Section -->
     <section id="therapy-pets" class="therapy-pets-section">
         <div class="container">
+    
+    <h2>This is Paws & People!</h2>
+            <div class="container">
+            <div class="pet-card">
+                    <img src="images/chav4.jpg" alt="Team Photo" style="width: 450px; height: auto; object-fit: cover;">
+                    <div class="pet-card-content">
+                        <h3 class="inspiring-sentence"> Chavinho! </h3>
+                    <p> A skilled and compassionate dog, trained specialist in providing emotional support and comfort with remarkable intuition and unwavering dedication to those in need.</p>
+                    </div>
+                </div>
+        
             <h2>Meet Our Therapy Pets</h2>
             <div class="pet-card-grid">
                 <!-- Pet Cards Here -->
@@ -25,6 +42,11 @@
         <div class="container">
             <h2>Meet Our Team</h2>
             <div class="pet-card">
+<img src="images/chav3.jpg" alt="Team Photo" style="width: 450px; height: auto; object-fit: cover;">
+                    <div class="pet-card-content">
+                    <p> At Paws & People Association, we believe in the healing power of the human-dog bond. Our organization focuses on leveraging the companionship of our trained dogs to provide comfort and support to those facing health challenges. Through therapy sessions, educational programs, and community outreach, we make a positive impact on health.</p>
+                    </div>
+   
                 <img src="images/chav4.jpg" alt="Team Photo 1"style="width: 450px; height: auto; object-fit: cover;">
                 <img src="images/chav3.jpg" alt="Description of Photo 3"style="width: 450px; height: 330px; object-fit: cover;">
             </div>
