5b2cdc61aec548c75db1356a1361d7bea3dce64c
leofer99 <44415118+leofer99@users.noreply.github.com>
2023-12-20 23:12:25 +0000

    Add files via upload

--- Project/database/insert.php
+++ Project/database/insert.php
@@ -1,10 +1,18 @@
 <?php
 
-function insertNewcomer($name, $email, $phone_number) {
+function insertNewcomer($name, $email, $phone_number, $gender, $city, $joined_date, $login_password) {
   try {
   global $dbh;
   $stmt = $dbh->prepare('INSERT INTO Person (name, email, phone_number) VALUES (?, ?, ?)');
-  return $stmt->execute(array($name, $email, $phone_number));
+  $stmt->execute(array($name, $email, $phone_number));
+
+  //Find the corresponding id_
+  $stmt = $dbh->prepare('SELECT id_ FROM Person WHERE name=? AND email=? AND phone_number=?;');
+  $stmt->execute(array($name, $email, $phone_number));
+  $id_=$stmt->fetchColumn(); 
+
+  $stmt = $dbh->prepare('INSERT INTO NonMember (id_, gender, city, joined_date, login_password) VALUES (?, ?, ?, ?, ?)');
+  return $stmt->execute(array($id_, $gender, $city, $joined_date, hash('sha256', $login_password)));
   
 } catch (PDOException $e) {
     $error_msg = $e->getMessage();
@@ -13,12 +21,31 @@ function insertNewcomer($name, $email, $phone_number) {
   }
 }
 
-//  INACABADA
-function insertUser($id_, $login_id, $password) {
+function insertUser($id_) {
     try {
+      //var_dump($id_);
+
     global $dbh;
-    $stmt = $dbh->prepare('INSERT INTO Member (id_, login_id, login_password) VALUES (?, ?, ?);');
-    $stmt->execute(array($login_id, hash('sha256', $password), $id_));
+    //Find the corresponding id_
+    $stmt = $dbh->prepare('SELECT gender, city, joined_date, login_password FROM NonMember WHERE id_=?;');
+    $stmt->execute(array($id_));
+    $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+    //var_dump($user_data);
+
+    $gender= $user_data['gender'];
+    $city= $user_data['city'];
+    $joined_date= $user_data['joined_date'];
+    $login_password= $user_data['login_password'];
+    
+    $stmt = $dbh->prepare('INSERT INTO Member (id_, gender, city, joined_date, login_id, login_password) VALUES (?, ?, ?, ?, ?, ?);');
+    $result= $stmt->execute(array($id_, $gender, $city, $joined_date, $id_, $login_password));
+    
+    $stmt = $dbh->prepare('DELETE FROM NonMember WHERE id_=?;');
+    $result= $stmt->execute(array($id_));
+
+    return $result;
+
+
   } catch (PDOException $e) {
     $error_msg = $e->getMessage();
     $_SESSION['msg'] = 'Error creating new user: ' . $error_msg;
